50eebe9f95f99dbdf0f4dfdabe73a49509aa66c5
iamWalo <azer44334433@gmail.com>
2026-01-30 20:13:18 +0100

    change maile sender

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,9 +1,10 @@
-import rateLimit from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
 
-export const formLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5, 
-  message: {
-    error: "Trop de requêtes, réessayez plus tard 🛑",
-  },
-});
+// export const formLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5, 
+//   message: {
+//     error: "Trop de requêtes, réessayez plus tard 🛑",
+//   },
+//    keyGenerator: (req) => req.ip,
+// });
--- src/routes/leads.route.js
+++ src/routes/leads.route.js
@@ -1,23 +1,17 @@
 import { Router } from "express";
-import transporter from "../config/mailer.js";
+import transporter from "../config/mailer1.js";
 import validateForm from "../utils/validate.js";
 import { formLimiter } from "../middlewares/rateLimiter.js";
 
 const router = Router();
 
-router.post('/', formLimiter, async (req, res) => {
+router.post("/", formLimiter, async (req, res) => {
   const error = validateForm(req.body);
   if (error) {
     return res.status(400).json({ error });
   }
 
   const { email, targetEmail, message } = req.body;
-
-  if (!email || !targetEmail || !message) {
-    return res.status(400).json({
-      message: "Please provide email, targetEmail and message",
-    });
-  }
 
   try {
     await transporter.sendMail({
@@ -27,20 +21,16 @@ router.post('/', formLimiter, async (req, res) => {
       html: `
         <h3>New Message</h3>
         <p><strong>From:</strong> ${email}</p>
-        <p><strong>To:</strong> ${targetEmail}</p>
         <p><strong>Message:</strong><br/>${message}</p>
       `,
     });
 
-    return res.json({
-      message: "Email sent successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: "Server error, try again later",
-    });
+    res.json({ message: "Email sent successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
 });
+
 
 export default router;
