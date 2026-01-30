686900fc3a08713fc6c11e09c8ab07d2d3b1ee6e
iamWalo <azer44334433@gmail.com>
2026-01-30 19:45:11 +0100

    change lesting func and the port

--- src/Server.js
+++ src/Server.js
@@ -4,11 +4,11 @@ import app from "./App.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Backend is live 🚀");
 });
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
